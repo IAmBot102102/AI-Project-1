515dc3ed54294ddc12804ac4e60faecb1b59cb2f
Comrade-Thotsky <tylerboyer0910@gmail.com>
2019-11-20 09:11:38 -0500

    lel
    lel

---
+++ LolAI/LolAI.cpp
@@ -0,0 +1,12 @@
+// LolAI.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <random>
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
