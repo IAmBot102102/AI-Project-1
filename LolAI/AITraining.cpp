4a3a54bb5d8c5d8b7a42be313ece604b7e666fa0
IAmBot102102 <53452898+IAmBot102102@users.noreply.github.com>
2019-11-21 05:09:41 -0500

    Updated Workspace

--- LolAI/AITraining.cpp
+++ LolAI/AITraining.cpp
@@ -2,8 +2,3 @@
 // Created by 1662978 on 11/20/2019.
 //
 
-#include <iostream>
-
-int CreateAI(){
-
-}
---
+++ LolAI/AITraining.h
@@ -0,0 +1,8 @@
+//
+// Created by 1662978 on 11/21/2019.
+//
+
+#ifndef AI_PROJECT_1_AITRAINING_H
+#define AI_PROJECT_1_AITRAINING_H
+int Jerome();
+#endif //AI_PROJECT_1_AITRAINING_H
